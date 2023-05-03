cf5ab415eef118c3e0b31d881aeb9e7218feb3e1
Woozir Manilall <dryshtee.woozir@accenture.com>
2023-05-03 09:24:57 +0400

    ajout commentaire

--- app-todos/src/App.js
+++ app-todos/src/App.js
@@ -2,9 +2,20 @@ import { useState } from "react";
 import './App.css';
 
 const App = () => {
+  /**
+   * useState hook is used to maintain track of the state inside the component - takes a 
+   * state's initial value and returns an array with two values:
+   * the first is a getter function that displays the defined state's current value
+   * the second is a setter function that updates the state
+   */
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
 
+  /**
+   * Important: the state should never be direectly mutated
+   * A copy of the Todos array, using the Array spread operator 
+   * is created before adding todo items to it.
+   */
   const addTodo = () => {
     if(todo !== ""){
       setTodos([...todos, todo]);
@@ -12,12 +23,22 @@ const App = () => {
     }
   };
 
+  /**
+   * onClick function triggers when delete button is clicked
+   * filter() - used to construct a new array with all the entries that meet the stated 
+   * criteria.
+   */
   const deleteTodo = (text) => {
     const newTodos = todos.filter((todo) => {
       return todo !== text;
     });
     setTodos(newTodos);
   }
+
+  /**
+   * ternary operator - used to display the list items + if list is empty
+   * map()- iterate over the elements in the array and display as <li> inside <ul> element
+   */
 
   return (
     <div className="App">
